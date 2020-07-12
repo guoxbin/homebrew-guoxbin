a74075738f1e1e2dc38da5453ccefc0fe204102c
GB <guoxbin@gmail.com>
2020-07-12 12:31:27 +0800

    dtool 0.10.0 (#8)

--- Formula/dtool.rb
+++ Formula/dtool.rb
@@ -1,8 +1,8 @@
 class Dtool < Formula
   desc "Command-line tool collection to assist development"
   homepage "https://github.com/guoxbin/dtool"
-  url "https://github.com/guoxbin/dtool/archive/v0.9.0.tar.gz"
-  sha256 "50cc164e9baf2262ae189c7986af3e1693479e708e38d6b6f36c6f8566593a22"
+  url "https://github.com/guoxbin/dtool/archive/v0.10.0.tar.gz"
+  sha256 "ce6cabd322050014f34bc135a80054c295dfc5df327b45f41f6aee40856d4665"
 
   depends_on "rust" => :build
 
